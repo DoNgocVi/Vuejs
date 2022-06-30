7777a34ce9038799b92d5b84360299ec9c5ce32a
vi <dn.VI@mor.com.vn>
2022-06-30 09:01:49 +0700

    format code

--- src/views/EditCity.vue
+++ src/views/EditCity.vue
@@ -1,67 +1,81 @@
 <template>
-   
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6 offset-md-3">
-                <h2>Update City</h2>
-                    <form @submit.prevent="updateCity">
-                        <div class="form-group">
-                            <input type="text" class="form-control mb-2"  aria-describedby="emailHelp" placeholder="City" v-model="cityInfo.name">
-                           
-                        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 offset-md-3">
+        <h2>Update City</h2>
+        <form @submit.prevent="updateCity">
+          <div class="form-group">
+            <input
+              type="text"
+              class="form-control mb-2"
+              aria-describedby="emailHelp"
+              placeholder="City"
+              v-model="cityInfo.name"
+            />
+          </div>
 
-                        <div class="form-group">
-                            <input type="text" class="form-control mb-2" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" v-model="cityInfo.county">
-                        </div>
+          <div class="form-group">
+            <input
+              type="text"
+              class="form-control mb-2"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+              v-model="cityInfo.county"
+            />
+          </div>
 
-                        <div class="form-group">
-                            <input type="text" class="form-control mb-2" placeholder="Country" v-model="cityInfo.country">
-                        </div>
-                      
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
-            </div>
-        </div>
+          <div class="form-group">
+            <input
+              type="text"
+              class="form-control mb-2"
+              placeholder="Country"
+              v-model="cityInfo.country"
+            />
+          </div>
+
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>
     </div>
-   
+  </div>
 </template>
 <script>
-import citiesColRef from '../firebase'
-import {getDoc, doc, setDoc} from 'firebase/firestore'
+import citiesColRef from "../firebase";
+import { getDoc, doc, setDoc } from "firebase/firestore";
 export default {
-    data(){
-        return{
-            selectedCity:{},
-            cityId: null,
-            docRef: null,
-            cityInfo:{
-                name: null,
-                country: null,
-                county: null
-            }
-        }
+  data() {
+    return {
+      selectedCity: {},
+      cityId: null,
+      docRef: null,
+      cityInfo: {
+        name: null,
+        country: null,
+        county: null,
+      },
+    };
+  },
+  methods: {
+    async getCity() {
+      this.docRef = doc(citiesColRef, this.cityId);
+      let city = await getDoc(this.docRef);
+      console.log(city.data());
+      let cityData = city.data();
+      this.cityInfo.name = cityData.name;
+      this.cityInfo.county = cityData.county;
+      this.cityInfo.country = cityData.country;
     },
-    methods:{
-        async getCity(){
-            this.docRef = doc(citiesColRef, this.cityId)
-            let city = await getDoc(this.docRef)
-            console.log(city.data());
-            let cityData = city.data()
-            this.cityInfo.name = cityData.name
-            this.cityInfo.county = cityData.county
-            this.cityInfo.country = cityData.country
-        },
-        async updateCity(){
-            await setDoc(this.docRef, this.cityInfo)
-            alert(`city ${this.cityId}, ${this.cityInfo}`)
-            this.$router.push('/')
-        }
-        
+    async updateCity() {
+      await setDoc(this.docRef, this.cityInfo);
+      alert(`city ${this.cityId}, ${this.cityInfo}`);
+      this.$router.push("/");
     },
-    created(){
-        let cityId = this.$route.params.cityId
-        this.cityId = cityId
-        this.getCity()
-    }
-}
+  },
+  created() {
+    let cityId = this.$route.params.cityId;
+    this.cityId = cityId;
+    this.getCity();
+  },
+};
 </script>
